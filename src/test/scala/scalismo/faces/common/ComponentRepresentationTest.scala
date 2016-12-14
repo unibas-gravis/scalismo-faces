b5abce5b7768274b4a949cd49610e268496931d7
Sandro Schönborn <sandro.schoenborn@unibas.ch>
2016-12-14 16:17:02 +0100

    added test for ComponentRepresentation

---
+++ src/test/scala/scalismo/faces/common/ComponentRepresentationTest.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016 University of Basel, Graphics and Vision Research Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package scalismo.faces.common
+
+import scalismo.faces.FacesTestSuite
+import scalismo.faces.color.{RGB, RGBA}
+import scalismo.geometry.{Vector, _2D, _3D}
+
+class ComponentRepresentationTest extends FacesTestSuite {
+
+  def testComponentRepresentation[A](expectedDimensionality: Int)(implicit vectorizer: ComponentRepresentation[A]): Unit = {
+    // dim
+    val d = vectorizer.size
+    it("should have the right dimensionality") {
+      d shouldBe expectedDimensionality
+    }
+
+    val data = Array.fill(d)(randomDouble)
+    val value = vectorizer.fromArray(data)
+
+    // toArray and fromArray
+    it("toArray and fromArray compose to identity") {
+      vectorizer.toArray(vectorizer.fromArray(data)).deep shouldBe data.deep
+    }
+
+    // intoArray and toArray
+    it("intoArray and fromArray are consistent") {
+      val test = new Array[Double](d)
+      vectorizer.intoArray(value, test)
+      test.deep shouldBe data.deep
+    }
+
+    // component and toArray
+    it("component(i) and fromArray are consistent") {
+      for (i <- 0 until d)
+        vectorizer.component(value, i) shouldBe data(i)
+    }
+
+    // fromComponents and fromArray
+    it("fromComponents and fromArray are consistent") {
+      def comp(i: Int): Double = data(i)
+      vectorizer.fromComponents(comp) shouldBe value
+    }
+  }
+
+  describe("A ComponentRepresentation[Vector[_2D]]") {
+    testComponentRepresentation[Vector[_2D]](2)
+  }
+
+  describe("A ComponentRepresentation[Vector[_3D]]") {
+    testComponentRepresentation[Vector[_3D]](3)
+  }
+
+  describe("A ComponentRepresentation[RGB]") {
+    testComponentRepresentation[RGB](3)
+  }
+
+  describe("A ComponentRepresentation[RGBA]") {
+    testComponentRepresentation[RGBA](4)
+  }
+}
