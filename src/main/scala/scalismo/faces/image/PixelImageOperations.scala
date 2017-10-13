9e80392aabb5bf0da8bd8d85be157f4d0692dc77
BernhardEgger <egger.bernhard@gmail.com>
2017-10-13 14:08:26 +0200

    adding mirroring capability to PixelImageOperations (#72)
    * adding mirroring
    
    * fixed pixel shift

--- src/main/scala/scalismo/faces/image/PixelImageOperations.scala
+++ src/main/scala/scalismo/faces/image/PixelImageOperations.scala
@@ -204,4 +204,15 @@ object PixelImageOperations {
     }
   }
 
+  /** Mirrors the target at the horizontally */
+  def mirrorHorizontal[Pixel](target: PixelImage[Pixel]): PixelImage[Pixel] = {
+    PixelImage(target.width, target.height, (x, y) => target(x, target.height - y - 1))
+  }
+
+  /** Mirrors the target at the vertically */
+  def mirrorVertical[Pixel](target: PixelImage[Pixel]): PixelImage[Pixel] = {
+    PixelImage(target.width, target.height, (x, y) => target(target.width - x - 1, y))
+  }
+
+
 }
