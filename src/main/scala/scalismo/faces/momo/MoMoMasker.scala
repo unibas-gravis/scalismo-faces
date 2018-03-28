802efcef80d2cc5852acb11fad58ead6cbee5a65
Andreas Schneider <andreas.schneider@unibas.ch>
2018-03-28 16:23:54 +0200

    use marginal for masking.

--- src/main/scala/scalismo/faces/momo/MoMoMasker.scala
+++ src/main/scala/scalismo/faces/momo/MoMoMasker.scala
@@ -16,38 +16,13 @@
 
 package scalismo.faces.momo
 
-import breeze.linalg.{DenseMatrix, DenseVector}
-import scalismo.common.{UnstructuredPointsDomain, Vectorizer}
-import scalismo.faces.color.RGB
-import scalismo.geometry.{Point, Vector, _3D}
 import scalismo.mesh.MeshCompactifier
-import scalismo.statisticalmodel.DiscreteLowRankGaussianProcess
 
-/** Mask a MoMo or DiscreteLowRankGaussianProcess according to supplied mask in the form of a MeshCompactifier.*/
+/** Mask a MoMo according to supplied mask in the form of a MeshCompactifier.*/
 case class MoMoMasker(op: MeshCompactifier) {
   val maskedMesh = op.transformedMesh
   val newIdx = maskedMesh.triangulation.pointIds
-
-  /* Mask a model by removing entries from the given model mean and eigenvectors and creating a new model from them. */
-  def maskDiscreteLowRankGaussianProcess[A](m: PancakeDLRGP[_3D, UnstructuredPointsDomain[_3D], A])(implicit vectorizer: Vectorizer[A]): DiscreteLowRankGaussianProcess[_3D, UnstructuredPointsDomain[_3D], A] = {
-
-    val domain = UnstructuredPointsDomain[_3D](maskedMesh.position.pointData)
-
-    val gp = m.gpModel
-    val meanNotMasked = gp.meanVector.toArray.grouped(vectorizer.dim).toIndexedSeq
-
-    val meanMasked = DenseVector(
-      newIdx.flatMap { id => meanNotMasked(op.pointBackMap(id).id) }.toArray
-    )
-
-    val maskedBasisMatrix = DenseMatrix.zeros[Double](newIdx.length * vectorizer.dim, m.gpModel.basisMatrix.cols)
-    newIdx.foreach { id =>
-      for (d <- 0 until vectorizer.dim) {
-        maskedBasisMatrix(id.id * vectorizer.dim + d, ::) := gp.basisMatrix(op.pointBackMap(id).id * vectorizer.dim + d, ::)
-      }
-    }
-    DiscreteLowRankGaussianProcess[_3D, UnstructuredPointsDomain[_3D], A](domain, meanMasked, gp.variance, maskedBasisMatrix)
-  }
+  val oldIds = newIdx.map(op.pointBackMap)
 
   def maskMoMo(model: MoMo): MoMo = {
     model match {
@@ -57,15 +32,15 @@ case class MoMoMasker(op: MeshCompactifier) {
   }
 
   def maskMoMoBasic(model: MoMoBasic): MoMoBasic = {
-    val maskedModelShape: PancakeDLRGP[_3D, UnstructuredPointsDomain[_3D], Point[_3D]] = PancakeDLRGP(maskDiscreteLowRankGaussianProcess(model.shape))
-    val maskedModelColor: PancakeDLRGP[_3D, UnstructuredPointsDomain[_3D], RGB] = PancakeDLRGP(maskDiscreteLowRankGaussianProcess(model.color))
+    val maskedModelShape = model.shape.marginal(oldIds)
+    val maskedModelColor = model.color.marginal(oldIds)
     MoMo(maskedMesh, maskedModelShape, maskedModelColor)
   }
 
   def maskMoMoExpress(model: MoMoExpress): MoMoExpress = {
-    val maskedModelShape: PancakeDLRGP[_3D, UnstructuredPointsDomain[_3D], Point[_3D]] = PancakeDLRGP(maskDiscreteLowRankGaussianProcess(model.shape))
-    val maskedModelColor: PancakeDLRGP[_3D, UnstructuredPointsDomain[_3D], RGB] = PancakeDLRGP(maskDiscreteLowRankGaussianProcess(model.color))
-    val maskedModelExpressions: PancakeDLRGP[_3D, UnstructuredPointsDomain[_3D], Vector[_3D]] = PancakeDLRGP(maskDiscreteLowRankGaussianProcess(model.expression))
+    val maskedModelShape = model.shape.marginal(oldIds)
+    val maskedModelColor = model.color.marginal(oldIds)
+    val maskedModelExpressions = model.expression.marginal(oldIds)
     MoMo(maskedMesh, maskedModelShape, maskedModelColor, maskedModelExpressions, model.landmarks)
   }
 }
