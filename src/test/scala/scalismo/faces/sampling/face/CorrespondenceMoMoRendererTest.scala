be5095ecd5f05c0b631a974cc04c13e136943cab
andreas-schneider <andreas.schneider@unibas.ch>
2018-03-29 15:02:00 +0200

    rendering of correspondence images (with backface culling) and tests. (#97)

---
+++ src/test/scala/scalismo/faces/sampling/face/CorrespondenceMoMoRendererTest.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright University of Basel, Graphics and Vision Research Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package scalismo.faces.sampling.face
+
+import java.net.URI
+
+import scalismo.faces.FacesTestSuite
+import scalismo.faces.color.RGBA
+import scalismo.faces.parameters.{MoMoInstance, Pose, RenderParameter}
+import scalismo.faces.utils.LanguageUtilities
+import scalismo.geometry.Vector
+
+class CorrespondenceMoMoRendererTest extends FacesTestSuite {
+  describe("A CorrespondenceMoMoRenderer") {
+    lazy val randomMomo = randomGridModel(10, 5, 0.1, 5, 5, orthogonalExpressions = false)
+    val moMoRenderer = MoMoRenderer(randomMomo).cached(0)
+    val corrMoMoRenderer = CorrespondenceMoMoRenderer(randomMomo).cached(0)
+    val param = RenderParameter.defaultSquare.
+      withMoMo(MoMoInstance.zero(randomMomo, new URI("randomModel"))).
+      withPose(Pose(1.0, Vector(0,0,-10), 0,0,0))
+
+    def diffRGBA(a: RGBA, b: RGBA) = math.abs(a.r - b.r) + math.abs(a.g - b.g) + math.abs(a.b - b.b) + math.abs(a.a - b.a)
+
+    it("renders the same as a MoMoRenderer") {
+      val A = moMoRenderer.renderImage(param)
+      val B = corrMoMoRenderer.renderImage(param)
+      val sum = A.zip(B).map{case(a,b)=>diffRGBA(a,b)}.values.toIndexedSeq.sum
+      sum should be <= 0.0
+    }
+
+    it("is approximately as fast as the MoMoRenderer") {
+      val t1 = LanguageUtilities.timed{for(i <- 0 until 5) moMoRenderer.renderImage(param)}._2
+      val t2 = LanguageUtilities.timed{for(i <- 0 until 5) corrMoMoRenderer.renderImage(param)}._2
+      t2 should be <= t1 * 1.5
+    }
+
+  }
+}
